982b207bd36e1f0eab785ab1ce9a8bbcdf80cfaa
Jose Alba Aguado <jaguado@google.com>
2020-08-14 16:23:07 +0200

    Fixed issues

--- adaptive_breakpoints/lib/adaptive_breakpoints_web.dart
+++ adaptive_breakpoints/lib/adaptive_breakpoints_web.dart
@@ -1,3 +1,7 @@
+// Copyright 2020, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:async';
 // In order to *not* need this ignore, consider extracting the "web" version
 // of your plugin as a separate package, instead of inlining it in the same
